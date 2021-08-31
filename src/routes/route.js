1e655dba3ae8f950ce583d5800b30119485f3968
Anthony Fleysher <antonfley@gmail.com>
2021-08-31 16:05:45 +0300

    routes added russian english

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,10 +3,13 @@ const price = require('../views/prices.json')
 const router = express.Router()
 
 router.route('/').get((req, res) => {
-	res.render('menu-hebrew',{price})
+	res.render('menu-hebrew', { price })
 })
 router.route('/english').get((req, res) => {
-	res.render('menu-english',{price})
+	res.render('menu-english', { price })
+})
+router.route('/russian').get((req, res) => {
+	res.render('menu-russian', { price })
 })
 
 module.exports = router
