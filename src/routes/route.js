15b8481f51b147e607874ce71cc894d08cf5f641
kartowka <antonfley@gmail.com>
2021-09-07 19:38:16 +0300

    update route to work with admin page and login

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,5 +12,25 @@ router.route('/english').get(price_controller.getPrices, (req, res) => {
 router.route('/russian').get(price_controller.getPrices, (req, res) => {
 	res.render('menu-russian', { pr: req.params.prices })
 })
+router
+	.route('/login')
+	.get(price_controller.denyIfLoggedin, (req, res) => {
+		res.render('login')
+	})
+	.post(price_controller.login, (req, res) => {
+		res.redirect(req.get('referer'))
+	})
+
+router
+	.route('/admin')
+	.get(
+		price_controller.allowIfLoggedin,
+		price_controller.grantAccess('readAny', 'price_settings'),
+		price_controller.getPrices,
+		(req, res) => {
+			res.render('admin', { pr: req.params.prices })
+		}
+	)
+	.post(price_controller.updatePrice)
 
 module.exports = router
