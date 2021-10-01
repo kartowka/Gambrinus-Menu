5ced28d022ad1d7067d19509c3d8457d59f2edcb
kartowka <antonfley@gmail.com>
2021-10-01 11:44:12 +0300

    reformatted all ejs files using prettier

--- src/routes/route.js
+++ src/routes/route.js
@@ -3,13 +3,16 @@ const router = express.Router()
 const price_controller = require('../controllers/price_controller')
 
 router.route('/').get(price_controller.getPrices, (req, res) => {
-	res.render('menu-hebrew', { pr: req.params.prices })
+	res.render('menus/menu-hebrew', { pr: req.params.prices })
 })
 router.route('/english').get(price_controller.getPrices, (req, res) => {
-	res.render('menu-english', { pr: req.params.prices })
+	res.render('menus/menu-english', { pr: req.params.prices })
 })
 router.route('/russian').get(price_controller.getPrices, (req, res) => {
-	res.render('menu-russian', { pr: req.params.prices })
+	res.render('menus/menu-russian', { pr: req.params.prices })
+})
+router.route('/homepage').get((req, res) => {
+	res.render('homepage')
 })
 router
 	.route('/login')
@@ -21,16 +24,11 @@ router
 	})
 router
 	.route('/edit_products')
-	.get(
-		price_controller.allowIfLoggedin,
-		price_controller.grantAccess('readAny', 'price_settings'),
-		price_controller.getPrices,
-		(req, res) => {
-			res.render('edit_products', {
-				pr: req.params.prices,
-			})
-		}
-	)
+	.get(price_controller.allowIfLoggedin, price_controller.grantAccess('readAny', 'price_settings'), price_controller.getPrices, (req, res) => {
+		res.render('edit_products', {
+			pr: req.params.prices,
+		})
+	})
 	.post(async (req, res) => {
 		if (Object.keys(req.body)[0] === 'edit_ID') {
 			res.redirect(`edit_products/${req.body.edit_ID}`)
@@ -41,41 +39,28 @@ router
 	})
 router
 	.route('/add_product')
-	.get(
-		price_controller.allowIfLoggedin,
-		price_controller.grantAccess('readAny', 'price_settings'),
-		(req, res) => {
-			res.render('add_product')
-		}
-	)
+	.get(price_controller.allowIfLoggedin, price_controller.grantAccess('readAny', 'price_settings'), (req, res) => {
+		res.render('add_product')
+	})
 	.post(async (req, res) => {
 		await price_controller.addProduct(req, res)
 		res.redirect(req.get('referer'))
 	})
 router
 	.route('/admin')
-	.get(
-		price_controller.allowIfLoggedin,
-		price_controller.grantAccess('readAny', 'price_settings'),
-		price_controller.getPrices,
-		(req, res) => {
-			res.render('admin_interface', {
-				user: req.user.username,
-			})
-		}
-	)
+	.get(price_controller.allowIfLoggedin, price_controller.grantAccess('readAny', 'price_settings'), price_controller.getPrices, (req, res) => {
+		res.render('admin_interface', {
+			user: req.user.username,
+		})
+	})
 	.post((req, res) => {})
 router
 	.route('/edit_products/:id')
-	.get(
-		price_controller.allowIfLoggedin,
-		price_controller.getPrice,
-		(req, res) => {
-			res.render('edit_product', {
-				pr: req.params.edit_ID,
-			})
-		}
-	)
+	.get(price_controller.allowIfLoggedin, price_controller.getPrice, (req, res) => {
+		res.render('edit_product', {
+			pr: req.params.edit_ID,
+		})
+	})
 	.post(price_controller.updatePrice, (req, res) => {
 		res.redirect(req.get('referer'))
 	})
